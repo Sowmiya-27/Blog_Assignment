b8597027d4002bc889b8a96cf0c410092a82dafd
sowmiya <sowmyarockfort@gmail.com>
2025-04-07 19:37:54 +0530

    first commit of blog project

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", views.blog_index, name="blog_index"),
+    path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("category/<category>/", views.blog_category, name="blog_category"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
